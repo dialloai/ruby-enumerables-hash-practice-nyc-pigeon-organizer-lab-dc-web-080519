86e867ce11aab1b6b1402a1a6b5ac49cf41e23bf
Aicha D <aicha1diallo@gmail.com>
2019-07-26 19:17:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,7 +19,7 @@
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |key, value|
+    data.each do |key, value|
     value.each do |new_value, names|
     names.each do |name|
       if !new_hash[name]
