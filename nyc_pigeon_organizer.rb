f3966ad47a3386760073f32eccdfb2e32b9f9aa0
Aicha D <aicha1diallo@gmail.com>
2019-07-26 19:22:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,8 +29,8 @@ def nyc_pigeon_organizer(data)
            new_hash[name][key] = []
         end      
           new_hash[name][key] << new_value.to_s
-          end
-          end 
-          end
-          new_hash
-          end
+    end
+  end 
+end
+new_hash
+end
