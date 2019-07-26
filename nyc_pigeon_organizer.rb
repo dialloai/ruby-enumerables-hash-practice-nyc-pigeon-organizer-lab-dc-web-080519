825a240e6a3b2e4cc8ffd7358e7cc082e59805cf
Aicha D <aicha1diallo@gmail.com>
2019-07-26 19:15:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,19 @@
-def nyc_pigeon_organizer(data)
- pigeons = {}
- data.each do |key, value| 
-  value.each do |new_value, names|
-    names.each do |name|
-      if !pigeons[name]         
-          pigeons[name] = {}      
-      end
+# def nyc_pigeon_organizer(data)
+# pigeons = {}
+# data.each do |key, value| 
+#   value.each do |new_value, names|
+#     names.each do |name|
+#       if !pigeons[name]         
+#           pigeons[name] = {}      
+#       end
       
-      if !pigeons[name][key]
-        pigeons[name][pigeons] = []
-      end
-      pigeons[name][key] << new_value.to_s
-    end
-  end
- end
- pigeons
-end
+#       if !pigeons[name][key]
+#         pigeons[name][pigeons] = []
+#       end
+#       pigeons[name][key] << new_value.to_s
+#     end
+#   end
+# end
+# pigeons
+# end
 
