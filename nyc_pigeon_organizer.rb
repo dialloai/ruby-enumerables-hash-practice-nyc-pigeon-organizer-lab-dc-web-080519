1e038522d22f3d08080eea7bf77298329283774e
Aicha D <aicha1diallo@gmail.com>
2019-07-26 19:18:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,36 +1,36 @@
-# def nyc_pigeon_organizer(data)
-# pigeons = {}
-# data.each do |key, value| 
-#   value.each do |new_value, names|
-#     names.each do |name|
-#       if !pigeons[name]         
-#           pigeons[name] = {}      
-#       end
-      
-#       if !pigeons[name][key]
-#         pigeons[name][pigeons] = []
-#       end
-#       pigeons[name][key] << new_value.to_s
-#     end
-#   end
-# end
-# pigeons
-# end
-
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-    data.each do |key, value|
-    value.each do |new_value, names|
+pigeons = {}
+data.each do |key, value| 
+  value.each do |new_value, names|
     names.each do |name|
-      if !new_hash[name]
-        new_hash[name] = {}
-        end      
-        if !new_hash[name][key]        
-          new_hash[name][key] = []
-          end      
-          new_hash[name][key] << new_value.to_s
-          end
-          end 
-          end
-          new_hash
-          end
+      if !pigeons[name]         
+          pigeons[name] = {}      
+      end
+      
+      if !pigeons[name][key]
+        pigeons[name][pigeons] = []
+      end
+      pigeons[name][key] << new_value.to_s
+    end
+  end
+end
+pigeons
+end
+
+# def nyc_pigeon_organizer(data)
+#   new_hash = {}
+#     data.each do |key, value|
+#     value.each do |new_value, names|
+#     names.each do |name|
+#       if !new_hash[name]
+#         new_hash[name] = {}
+#         end      
+#         if !new_hash[name][key]        
+#           new_hash[name][key] = []
+#           end      
+#           new_hash[name][key] << new_value.to_s
+#           end
+#           end 
+#           end
+#           new_hash
+#           end
